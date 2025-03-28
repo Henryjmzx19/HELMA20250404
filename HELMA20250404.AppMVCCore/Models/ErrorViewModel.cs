8e482838b239cee9dab23c780d53bfcfbb9643c6
Henry Jimenez <henryjmzx19@hotmail.com>
2025-03-27 22:33:51 -0600

    Agregar archivos de proyecto.

---
+++ HELMA20250404.AppMVCCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HELMA20250404.AppMVCCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
