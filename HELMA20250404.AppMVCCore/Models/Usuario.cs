04f13c75e2026c4e2121f6e1c96cab8b69a85273
Eneida Anzora <anzoraeneida@outlook.com>
2025-03-28 22:14:27 -0600

    Se agregaron las validaciones al Modelo y se creo el Controlador para Usuarios

--- HELMA20250404.AppMVCCore/Models/Usuario.cs
+++ HELMA20250404.AppMVCCore/Models/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace HELMA20250404.AppMVCCore.Models;
 
@@ -7,12 +8,20 @@ public partial class Usuario
 {
     public int Id { get; set; }
 
+    [Display(Name = "Nombre de usuario")]
+    [Required(ErrorMessage = "El nombre de usuario es obligatorio.")]
     public string NombreUsuario { get; set; } = null!;
 
+    [EmailAddress]
+    [Required(ErrorMessage = "El email es obligatorio.")]
     public string Email { get; set; } = null!;
 
+    [Display(Name = "Contraseña")]
+    [Required(ErrorMessage = "La contraseña es obligatorio.")]
+    [StringLength(100, MinimumLength = 5, ErrorMessage = "El password debe tener entre 5 y 100 caracteres.")]
     public string Password { get; set; } = null!;
 
+    [Required(ErrorMessage = "El rol es obligatorio.")]
     public string Rol { get; set; } = null!;
 
     public virtual Alumno? Alumno { get; set; }
